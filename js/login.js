5e914109068e3de710e0dfdab3c3a23d14ced74e
unknown <yaraomar238@gmail.com>
2025-12-29 07:24:06 +0200

    finish login page

--- js/login.js
+++ js/login.js
@@ -1,24 +1,48 @@
 ///login js
 let loginbutton = document.getElementById("login");
-console.log(loginbutton);
+ let emailinput = document.getElementById("Email");
+let passwordinput = document.getElementById("Password");
 
+console.log(loginbutton);
+  emailinput.addEventListener("input", () => {
+            successmessage.textContent = "";
+        });
+
+        passwordinput.addEventListener("input", () => {
+            successmessage.textContent = "";
+        });
 
 if (loginbutton) {
     loginbutton.addEventListener("click", function () {
         let storedEmail = localStorage.getItem("Email");
         let storedPassword = localStorage.getItem("Password");
         let successmessage = document.getElementById("successmessage");
+        
+    
 
         let emailinput = document.getElementById("Email").value;
         let passwordinput = document.getElementById("Password").value;
+      
+        console.log(emailinput);
+        if (!emailinput || !passwordinput) {
+            successmessage.textContent = "Please fill in all fields!";
+            successmessage.style.color = "red";
+            successmessage.style.fontWeight = "bold";
+            return;
+        }
+        
 
         if (emailinput === storedEmail && passwordinput === storedPassword) {
             successmessage.textContent = "Login successful";
-        } else {
-            successmessage.textContent = "Invalid email or password ";
-        }
-        setInterval(() => {
+             setInterval(() => {
             location.href = "index.html";
         }, 1500);
+            
+        } else {
+            successmessage.textContent = "Invalid email or password ";
+            successmessage.style.color = "red";
+            successmessage.style.fontWeight = "bold";
+        }
+       
     });
 }
